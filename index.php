480bdf06008beb247130d512872de9129d4c09b6
Sandra <sandradjafer7@gmail.com>
2021-02-03 15:29:33 +0100

    commit

--- index.php
+++ index.php
@@ -150,7 +150,7 @@ foreach ($entriesTrans["raffinement"] as $key => $value) {
         $defRaff = getDefinition($pageRaff);
         if(isset($defRaff)){
         $entriesTrans["definition"].="<br>".$defRaff;
-        $lienR = "<a href='http://localhost/e-application/index.php?terme=".$value."' class='raff'>".$value."</a><br>";
+        $lienR = "<a href='https://indico-responsive.herokuapp.com/index.php?terme=".$value."' class='raff'>".$value."</a><br>";
     }
     $AffichageRaff .= $lienR;
 }
